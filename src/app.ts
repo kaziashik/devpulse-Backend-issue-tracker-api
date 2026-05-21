35b0bad2b220944381f965b8afe00021ba75a673
kaziashik <ashik@apppay.tech>
2026-05-21 18:46:11 +0800

    feat: create users and issues databse schema

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-
-
-import express, { type Application, type Request, type Response } from "express"
-export const app: Application=express()
-
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+export const app: Application = express();
 
 // const port=config.port;
 
-app.get('/',(req: Request, res:Response)=>{
-    res.status(200).json({
-        success:true,
-        message:"Server Home Route",
-        data: {}
-    })
-})
-
-
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Server Home Route",
+    data: {},
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,18 @@
 import express, { type Application, type Request, type Response } from "express"
 import { app } from "./app";
 import { config } from "./config";
+import { DBtable } from "./db";
 
 
 
 const prot= config.port
 
 const main=()=>{
+   
 
     app.listen(prot,()=>{
     console.log(`Server Runing on Port ${7000}`);
+     DBtable()
 })
 
 }
